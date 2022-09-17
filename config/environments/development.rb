ae8296b97fc72afc91107db298b86f7396e7fe13
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-09-17 08:42:43 +0000

    [finish]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "b7b1c4bc8d454f5198bc4ecc0ba195cf.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.quete_adapter=:inline
 end
